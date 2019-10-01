b68ee4d3db99a76765ab04c39f400cc9a568dd64
Дмитрий <daniel24111993@mail.ru>
2019-10-01 17:04:09 +0300

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function zeros(expression) {
-  // your solution
+   var index = expression.indexOf('!');
+  var string = '';
+  for (var i = 0; i < index; i++) {
+    string = string + expression[i];
+    string = parseInt(string, 10);
 }
+ var total = string;
+  for (var i = string - 1; i > 0; i--) {
+    total = total * i;
+  }
+  total = String(total);
+
+var sum = 0;
+var position = total.length - 1;
+
+
+while (total[position] === '0') {
+  sum = sum + 1;
+  position--;
+}
+return sum;
+};
